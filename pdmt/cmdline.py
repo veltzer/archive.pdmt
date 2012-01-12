eec173436874ba6b000bca88d62a54a58b907c43
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-12 11:02:17 +0200

    reorg

--- pdmt/cmdline.py
+++ pdmt/cmdline.py
@@ -1,37 +1,50 @@
-import optparse
+import argparse
+import utils.installer 
 def parse(mgr):
-	parser=optparse.OptionParser()
-	parser.add_option(
-			"-c","--clean",
-			help="clean project",
-			action="store_true",
+	parser=argparse.ArgumentParser(description='Project Dependency Management Tool')
+	parser.add_argument(
+			'--clean',
+			help='clean project',
+			action='store_true',
 			default=False,
 	)
-	parser.add_option(
-			"-b","--build",
-			help="build project",
-			action="store_true",
+	parser.add_argument(
+			'--build',
+			help='build project',
+			action='store_true',
 			default=False,
 	)
-	parser.add_option(
-			"-p","--prnt",
-			help="print graph",
-			action="store_true",
+	parser.add_argument(
+			'--dump',
+			help='dump graph',
+			action='store_true',
 			default=False,
 	)
-	parser.add_option(
-			"-i","--interactive",
-			help="start interactive mode",
-			action="store_true",
+	parser.add_argument(
+			'--install',
+			help='install',
+			action='store_true',
 			default=False,
 	)
-	(options,args)=parser.parse_args()
-	if sum([options.clean,options.build,options.prnt])>1:
-		parser.error("clean build and prnt are mutually exclusive")
-	#print options, args
+	parser.add_argument(
+			'--interactive',
+			help='start interactive mode',
+			action='store_true',
+			default=False,
+	)
+	options=parser.parse_args()
+	if sum([
+		options.clean,
+		options.build,
+		options.dump,
+		options.install,
+	])!=1:
+		parser.error('must specify one of clean,build,dump,install')
 	if options.clean:
 		mgr.clean()
 	if options.build:
 		mgr.build()
-	if options.prnt:
-		mgr.prnt()
+	if options.dump:
+		mgr.dump()
+	if options.install:
+		utils.installer.doit()
---
+++ pdmt/utils/installer.py
@@ -0,0 +1,12 @@
+"""
+This module installs dependencies for a package. Dependencies are gotten via
+configuration
+"""
+
+import subprocess
+import config
+
+def doit():
+	args=['sudo','apt-get','install']
+	args.extend(config.ns_install.p_deps)
+	subprocess.check_call(args)

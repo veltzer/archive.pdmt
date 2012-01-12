111939914241c43f2475ed7ad3d09ea3891356fa
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-12 11:56:36 +0200

    yet another attempt

--- pdmt/utils/debmaker.py
+++ pdmt/utils/debmaker.py
@@ -6,7 +6,7 @@ def doit():
 	out=subprocess.check_output(['git','status','-s'])
 	if out!='':
 		raise ValueError('first commit everything, then call me...')
-	subprocess.check_output(['setup.py','sdist'])
+	subprocess.check_output(['python','setup.py','sdist'])
 	name=config.ns_install.p_name+'-'+config.ns_install.p_version
 	subprocess.check_output(['py2dsc','dist/'+name+'.tar.gz'])
 	# save the current directory

fadbc3595e07701b1a98b0ba5ee211968f7410ac
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-12 23:47:34 +0200

    yet another try

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import pdmt.config # for configuration
 import distutils.core # for setup
 
 distutils.core.setup(
-	name=pdmt.config.ns_install.p_name,
-	description=pdmt.config.ns_install.p_description,
-	author=pdmt.config.ns_install.p_author,
-	author_email=pdmt.config.ns_install.p_email,
-	url=pdmt.config.ns_install.p_url,
-	version=pdmt.config.ns_install.p_version,
-	classifiers=pdmt.config.ns_install.p_classifiers,
-	requires=pdmt.config.ns_install.p_require,
-	packages=pdmt.config.ns_install.p_dir_list,
-	scripts=pdmt.config.ns_install.p_scripts,
-	data_files=pdmt.config.ns_install.p_data_files,
+	name=pdmt.config.ns_product.p_name,
+	description=pdmt.config.ns_product.p_description,
+	author=pdmt.config.ns_person.p_name,
+	author_email=pdmt.config.ns_person.p_email,
+	url=pdmt.config.ns_distrib.p_url_website,
+	version=pdmt.config.ns_product.p_version,
+	classifiers=pdmt.config.ns_product.p_classifiers,
+	requires=pdmt.config.ns_product.p_require,
+	packages=pdmt.config.ns_product.p_dir_list,
+	scripts=pdmt.config.ns_product.p_scripts,
+	data_files=pdmt.config.ns_product.p_data_files,
 )

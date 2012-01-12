77a9fdb31416446bb5ad42b544149a4b2d6faa25
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-12 11:55:29 +0200

    more typos

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import distutils.core # for setup
 
 distutils.core.setup(
 	name=config.ns_install.p_name,
-	description=config.ns.install.p_description,
+	description=config.ns_install.p_description,
 	author=config.ns_install.p_author,
-	author_email=config.ns_insta.p_email,
+	author_email=config.ns_install.p_email,
 	url=config.ns_install.p_url,
 	version=config.ns_install.p_version,
 	classifiers=config.ns_install.p_classifiers,

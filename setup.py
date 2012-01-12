19f06485994a60cf19aa7e06fa7e8868fde5607d
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-12 11:54:31 +0200

    typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ distutils.core.setup(
 	description=config.ns.install.p_description,
 	author=config.ns_install.p_author,
 	author_email=config.ns_insta.p_email,
-	url=config.ns_install.p_url
+	url=config.ns_install.p_url,
 	version=config.ns_install.p_version,
 	classifiers=config.ns_install.p_classifiers,
 	requires=config.ns_install.p_require,

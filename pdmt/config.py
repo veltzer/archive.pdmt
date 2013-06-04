a7c54930dd75f31e81ef01eb5333d3613afc9f8e
Mark Veltzer <mark@veltzer.net>
2013-06-04 09:03:51 +0300

    added some ideas

--- pdmt/config.py
+++ pdmt/config.py
@@ -9,9 +9,11 @@ if os.path.isfile(overridefiles):
 
 def show():
 	for ns_name in pdmt.config.__dict__:
+		#if not ns_name.startswith('__'):
 		if ns_name.startswith('ns_'):
 			print(ns_name)
 			ns=pdmt.config.__dict__[ns_name]
 			for p in ns.__dict__:
+				#if not p.startswith('__'):
 				if p.startswith('p_'):
 					print('\t',p,ns.__dict__[p])

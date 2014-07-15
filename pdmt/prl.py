82e2d69e5b5228af8db2e465ed9b1beb3eb0f85b
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-15 12:02:48 +0300

    prl -> prn

--- pdmt/prl.py
+++
@@ -1,9 +0,0 @@
-'''
-pdmt resource mgmt utils
-'''
-
-def create(name=None, proto=None):
-	return '{proto}://{name}'.format(
-		proto=proto,
-		name=name
-	)
---
+++ pdmt/prn.py
@@ -0,0 +1,17 @@
+'''
+prn==Pdmt Resource name
+pdmt resource mgmt utils
+'''
+
+def create(name=None, proto=None):
+	return '{proto}://{name}'.format(
+		proto=proto,
+		name=name
+	)
+
+def get_name_from_prn(s):
+	raise ValueError('implement me')
+def get_proto_from_prn(s):
+	raise ValueError('implement me')
+def get_name_proto_from_prn(s):
+	raise ValueError('implement me')

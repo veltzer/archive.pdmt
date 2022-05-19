748fd84ec9903f83f927d6c9eb3148a2486cb35a
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 19:12:37 +0300

    new config

--- config/python.py
+++ config/python.py
@@ -1,15 +1,6 @@
-import config.project
-
-package_name = config.project.name
-
 config_requires = [
     "pyclassifiers",
 ]
 make_requires = [
     "pymakehelper",
 ]
-
-python_requires = ">=3.10"
-
-test_os = ["ubuntu-22.04"]
-test_python = ["3.10"]
